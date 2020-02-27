c0aaa864323bfa8c00ec38efb293e48e5319063b
Emma Romero <emmayrs17@gmail.com>
2020-02-27 01:23:40 -0600

    GET INSERT & SEARCH

--- application/controllers/alumnos.py
+++ application/controllers/alumnos.py
@@ -53,11 +53,34 @@ class Alumnos:
                     result=[]
                     result.append(resulta)
                     inf['alumno']=result
-                    
                     with open('static/csv/alumnos.csv','a+', newline='') as variable_cualquiera:
                         writer=csv.writer(variable_cualquiera)
                         writer.writerow(res)
                     return json.dumps(inf)
+
+                if(data['action']=='buscar'):
+                    inf={}
+                    inf['version']="0.01"
+                    inf['status']="200 ok"
+                    matricula=str(data['matricula'])
+                    result="matricula,nombre,primer_apellido,segundo_apellido,carrera\n"
+                    with open('static/csv/alumnos.csv', 'r') as csvfile:
+                        reader = csv.DictReader(csvfile)
+                        result=[]
+                        for row in reader:
+                            if(row['matricula']==matricula):  
+                                resulta={}
+                                resulta['matricula']=str(row['matricula'])
+                                resulta['nombre']=str(row['nombre'])
+                                resulta['primer_apellido']=str(row['primer_apellido'])
+                                resulta['segundo_apellido']=str(row['segundo_apellido'])
+                                resulta['carrera']=str(row['carrera'])
+                                result.append(resulta)
+                                inf['alumno']=result
+                            if(result==[]):
+                                inf['status']="no existe este registro"
+                    return json.dumps(inf)
+
         except Exception as e:
             result=[]    
             result.append('error'+ str(e.args))
