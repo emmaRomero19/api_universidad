f756f16087f32eec5a644f816f439485318903c0
Emma Romero <emmayrs17@gmail.com>
2020-03-29 19:40:30 -0600

    Terminado

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ urls = (
 app = web.application(urls, globals())
 class Hello:
     def GET(self):
-        return "Hola Yuri\ntoken=1234\nConsultar ?action=get\nBuscar un registro por matricula ?action=search&matricula=xxx\nInsertar un nuevo registro, ?action=put&matricula=xxx&nombre=xxxx&primer_apellido=xxxx&segundo_apellido=xxxx&carrera=xxxx\nBorrar un registro, ?action=delete&matricula=xxxx\nActualizar datos, ?action=update&matricula=xxx&nombre=xxxx&primer_apellido=xxxx&segundo_apellido=xxxx&carrera=xxxx"
+        return "Hola Yuri\ntoken=1234\nConsultar /alumnos/?action=get\nBuscar un registro por matricula /alumnos/?action=search&matricula=xxx\nInsertar un nuevo registro, /alumnos/?action=put&matricula=xxx&nombre=xxxx&primer_apellido=xxxx&segundo_apellido=xxxx&carrera=xxxx\nBorrar un registro, /alumnos/?action=delete&matricula=xxxx\nActualizar datos, /alumnos/?action=update&matricula=xxx&nombre=xxxx&primer_apellido=xxxx&segundo_apellido=xxxx&carrera=xxxx"
 if __name__ == "__main__":
     app.run()
